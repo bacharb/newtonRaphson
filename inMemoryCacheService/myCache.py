942eb07036ad1656c44f94fa4d033802b4a4514e
bachar <bachar.bouazza@gmail.com>
2017-02-14 12:37:12 -0500

    The cache class  is put in myCache.py file

---
+++ inMemoryCacheService/myCache.py
@@ -0,0 +1,36 @@
+import pylru
+
+class inMemoryCache(object):
+    def __init__(self, size=3, cacheMgr=pylru.WriteThroughCacheManager):
+        self._store = {}
+        self._cache = cacheMgr(self._store, size)
+        self._cacheMgr = cacheMgr
+
+    def __len__(self):
+        return len(self._cache)
+
+    def __contains__(self, key):
+        return key in self._cache
+
+    def __getitem__(self, key):
+        if key in self._cache:
+            return self._cache[key]
+        return None
+
+    def __setitem__(self, key, value):
+        self._cache[key] = value
+
+    def __delitem__(self, key):
+        if key in self._cache:
+            del self._cache[key]
+
+    def keys(self):
+        keys = set(self._cache.keys())
+        return keys
+
+    def items(self):
+        return dict(self._cache.items())
+
+    def clear(self):
+        self._cache.clear()
+        self._store = {}
--- inMemoryCacheService/restService.py
+++ inMemoryCacheService/restService.py
@@ -1,46 +1,12 @@
 #!/usr/bin/env python
 
-import pylru
 import web
 import datetime
 import re
 
-class inMemoryCache (object) :
-	def __init__ (self, size= 3, cacheMgr = pylru.WriteThroughCacheManager ):  
-		self._store = {}
-		self._cache = cacheMgr(self._store, size)
-		self._cacheMgr = cacheMgr
-    
-	def __len__(self) :
-		return len(self._cache)
+import myCache
 
-	def __contains__(self, key) :
-		return key in self._cache
-
-	def __getitem__(self, key) :
-		if key in self._cache:
-			return self._cache[key]
-		return None
-
-	def __setitem__(self, key, value) :
-		self._cache[key] = value
-            
-	def __delitem__(self, key) :
-		if key in self._cache :
-			del self._cache[key]
-         
-	def keys(self) :
-		keys = set(self._cache.keys())
-		return keys
-
-	def items(self) :
-		return dict(self._cache.items())
-	
-	def clear(self) : 
-		self._cache.clear()
-		self._store ={}
-
-store = inMemoryCache ()
+store = myCache.inMemoryCache ()
 
 urls = (
 			'/list', 				'listKeys',		# Dump the whole store
