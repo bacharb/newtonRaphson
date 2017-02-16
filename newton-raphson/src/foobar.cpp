1ed7d9980ceaf74c9e471afadace9bccdc03c863
bachar <bachar.bouazza@gmail.com>
2017-02-16 09:51:54 -0500

    :wq

--- newton-raphson/src/foobar.cpp
+++ newton-raphson/src/foobar.cpp
@@ -1,4 +1,20 @@
-	
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include <tuple>
+
+using namespace std;
+using Real=float;
+using point_2 = tuple<Real,Real> ;
+
 int main(){
-	return 0;
+  vector <point_2> v;
+  for (int i=0; i<10; i++){
+    v.push_back(point_2(i*2, i*2+1) );
+  }
+
+  for(auto t: v){
+    cout << get<0>(t) << ' ';
+    cout << get<1>(t) << ' ';
+  };
 }
