0d9ca51bce756148609d47651f3705c5223b0415
toto <toto@Bachars-MacBook-Air.local>
2017-02-20 11:25:24 -0500

    Deleted duplicated files .hpp from include dir.

--- newton-raphson/include/mainNR.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <functional>
-#include <topologyStrategy.hpp>
-#include <derivative.hpp>
-#include <newton-raphson.hpp>
-#include <types.hpp>
--- newton-raphson/include/types.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <functional>
-
-using Real     = double;   
-using Natural  = int;
-using Function = const std::function<double(double)> ;
-
